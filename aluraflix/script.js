e2c9027809e970f0bca93dd1946be92086eadc59
lucaspires-source <pires.lucas94@gmail.com>
2021-04-01 15:01:22 -0300

    aula 05

--- aluraflix/script.js
+++ aluraflix/script.js
@@ -1,14 +1,28 @@
+ 
+const adicionarFilme = () =>{
+    
+    const campoFilmeFavorito = document.querySelector('#filme')
+    const filmeFavorito = campoFilmeFavorito.value
 
-const filmes = [
-    "https://m.media-amazon.com/images/M/MV5BMzFkODkwN2ItMTk1NS00OWZiLTllNmMtYzBiNmY1OTk2YzI0XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UY268_CR3,0,182,268_AL_.jpg", 
-    "https://m.media-amazon.com/images/M/MV5BMjIxMTg5NjMzNl5BMl5BanBnXkFtZTcwMzgzMjI5NA@@._V1_UX182_CR0,0,182,268_AL_.jpg", 
-    "https://m.media-amazon.com/images/M/MV5BODAyMjQ4NzE1OF5BMl5BanBnXkFtZTcwMTQzMDYwOQ@@._V1_UX182_CR0,0,182,268_AL_.jpg", 
-    "https://m.media-amazon.com/images/M/MV5BZjc4NDZhZWMtNGEzYS00ZWU2LThlM2ItNTA0YzQ0OTExMTE2XkEyXkFqcGdeQXVyNjUwMzI2NzU@._V1_UY268_CR1,0,182,268_AL_.jpg",
-]
-
-
-
-for (let i = 0; i < filmes.length; i++) {
-    document.write("<img src="+filmes[i]+"/>")
-
+    if(filmeFavorito.endsWith('.jpg')){
+        listarFilmesNaTela(filmeFavorito)
+        
+    }else{
+        alert("Imagem invalida,somente urls de .jpg são aceitas")
+        
+    }
+    campoFilmeFavorito.value = ""
 }
+
+const listarFilmesNaTela = (filme) =>{
+    const listaFilmes = document.querySelector('#listaFilmes')
+    const elementoFilme = "<img src="+filme+"/>"
+    listaFilmes.innerHTML = listaFilmes.innerHTML +elementoFilme
+}
+
+const resetarLista = () =>{
+    const listaFilmes = document.querySelector('#listaFilmes')
+    listaFilmes.innerHTML = ""
+    campoFilmeFavorito.value = ""
+}
+
